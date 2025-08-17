8aca77536017f8e5b291fec16642914a4a4866c1
hungnguyen16112003 <nguyenquochung16112003@gmail.com>
2025-08-17 19:54:59 +0700

    update types

---
+++ src/types/types.d.ts
@@ -0,0 +1,16 @@
+interface IProductTypes {
+  data: {
+    href: string;
+    urlImage: string;
+  }[];
+}
+
+interface IBrandProps {
+  data: {
+    id: number;
+    href: string;
+    image: string;
+    width: number;
+    height: number;
+  }[];
+}
